7ffbd477e98f97c2d48d812d2a1e60802c89e1cf
Lautaro Blanco <lblanco@getontop.com>
2022-08-26 16:50:34 -0300

    Commit inicial.

---
+++ src/main/java/ar/edu/unq/desapp/grupoj/desapp/DesappApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoj.desapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoj/desapp/DesappApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoj.desapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
