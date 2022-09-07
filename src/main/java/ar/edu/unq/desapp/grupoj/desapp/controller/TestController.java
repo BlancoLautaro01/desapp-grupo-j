9008d3930962be0f13477b6932fe026b7ba508b6
Lautaro Blanco <lblanco@getontop.com>
2022-09-07 19:06:04 -0300

    Swagger implemented.

--- src/main/java/ar/edu/unq/desapp/grupoj/desapp/controller/TestController.java
+++ src/main/java/ar/edu/unq/desapp/grupoj/desapp/controller/TestController.java
@@ -1,13 +1,20 @@
 package ar.edu.unq.desapp.grupoj.desapp.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
+@Api(value = "", tags={"Test Controller"})
+@Tag(name = "Test Controller", description = "Test Methods")
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping
     public String test() {
         return "Hello World!";
     }
 }
+
