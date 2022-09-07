24af81af45b112c5b98abc5f1343a692308de914
Lautaro Blanco <lblanco@getontop.com>
2022-09-07 20:55:48 -0300

    Configure herokus deploy.

--- src/test/java/ar/edu/unq/desapp/grupoj/desapp/DesappApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupoj/desapp/DesappApplicationTests.java
@@ -1,13 +1,15 @@
 package ar.edu.unq.desapp.grupoj.desapp;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DesappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
